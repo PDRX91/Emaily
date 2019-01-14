5de0a309a8efc212f09a6e7c5904c9438f1a0dc4
PDRX91 <parker.rebensdorf@gmail.com>
2019-01-14 13:18:44 -0800

    header now changes based on logged in user or not

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
 	renderContent() {
 		switch (this.props.auth) {
 			case null:
-				return 'Still deciding';
+				return;
 			case false:
-				return 'Im logged out';
+				return (
+					<li>
+						<a href="/auth/google">Login With Google</a>
+					</li>
+				);
 			default:
-				return 'Im logged in';
+				return (
+					<li>
+						<a href="/api/logout">Logout</a>
+					</li>
+				);
 		}
 	}
 	render() {
 		return (
 			<nav>
 				<div className="nav-wrapper">
-					<a href="/" className="left brand-logo">
+					<Link
+						to={this.props.auth ? '/surveys' : '/'}
+						className="left brand-logo"
+					>
 						Emaily
-					</a>
+					</Link>
 					<ul className="right">{this.renderContent()}</ul>
 				</div>
 			</nav>
