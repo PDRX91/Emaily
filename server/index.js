949fede95197b05f7bbb74fafaad5949d241ec45
PDRX91 <parker.rebensdorf@gmail.com>
2019-01-21 14:57:43 -0800

    setting up charge handler with stripe

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
 const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(
+	keys.mongoURI,
+	{useNewUrlParser: true}
+);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -20,8 +25,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-//this is a useless addition so I can commit again.
