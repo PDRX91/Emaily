6d7121926a9d4e14c54a3746eb13e736af296322
PDRX91 <parker.rebensdorf@gmail.com>
2019-02-03 10:39:04 -0800

    added publishable key for stripe

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -2,6 +2,8 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 const requireLogin = require('../middlewares/requireLogin');
 
+stripe.setPublishableKey(keys.stripePublishableKey);
+
 module.exports = (app) => {
 	app.post('/api/stripe', requireLogin, async (req, res) => {
 		const charge = await stripe.charges.create({
