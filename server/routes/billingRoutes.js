b2e613d57b00213adaa43284cfe7888960ffcba2
PDRX91 <parker.rebensdorf@gmail.com>
2019-02-03 11:17:56 -0800

    reverting changes to stripe flow

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -2,8 +2,6 @@ const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 const requireLogin = require('../middlewares/requireLogin');
 
-stripe.setPublishableKey(keys.stripePublishableKey);
-
 module.exports = (app) => {
 	app.post('/api/stripe', requireLogin, async (req, res) => {
 		const charge = await stripe.charges.create({
