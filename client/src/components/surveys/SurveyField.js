adc2a4217a2eee8a8fd434cc1eb2631080bfe1a5
PDRX91 <parker.rebensdorf@gmail.com>
2019-02-17 18:29:55 -0800

    added individual field component to survey

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,11 @@
+//contains the logic to make a single input with label
+
+import React from 'react';
+
+export default ({input}) => {
+	return (
+		<div>
+			<input {...input} />
+		</div>
+	);
+};
