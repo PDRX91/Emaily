bdd297d562c88da192fc1222e4d874ad1dbb3d31
PDRX91 <parker.rebensdorf@gmail.com>
2019-02-28 20:36:03 -0800

    minor style changes to header coloring and spacing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -31,10 +31,11 @@ class Header extends Component {
 	render() {
 		return (
 			<nav>
-				<div className="nav-wrapper">
+				<div className="nav-wrapper blue darken-4">
 					<Link
 						to={this.props.auth ? '/surveys' : '/'}
 						className="left brand-logo"
+						style={{margin: '0 15px'}}
 					>
 						Emaily
 					</Link>
