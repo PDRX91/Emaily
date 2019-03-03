943c05a2ccdbdad0fffe34620ebdd677dee0d566
PDRX91 <parker.rebensdorf@gmail.com>
2019-03-03 14:29:09 -0800

    fixed front end naming mismatch

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -34,11 +34,11 @@ class SurveyForm extends Component {
 function validate(values) {
 	const errors = {};
 
-	errors.emails = validateEmails(values.emails || '');
+	errors.recipients = validateEmails(values.recipients || '');
 
 	_.each(formFields, ({name}) => {
 		if (!values[name]) {
-			errors[name] = `You must provide ${name === 'emails' ? 'recipients' : 'a ' + name}`;
+			errors[name] = `You must provide ${name === 'recipients' ? 'recipients' : 'a ' + name}`;
 		}
 	});
 
