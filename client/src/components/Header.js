87366b71b8c5ede659fc5b99965efce093d4e429
PDRX91 <parker.rebensdorf@gmail.com>
2019-03-09 18:18:05 -0800

    changing colors for design purposes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -31,7 +31,7 @@ class Header extends Component {
 	render() {
 		return (
 			<nav>
-				<div className="nav-wrapper blue darken-4">
+				<div className="nav-wrapper blue-grey darken-3">
 					<Link
 						to={this.props.auth ? '/surveys' : '/'}
 						className="left brand-logo"
